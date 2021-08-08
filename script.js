05c8ecd34dbda9c515bfab55d9ed9be4b67fd570
Giovanni Monsanto <monsanto.g@chello.nl>
2021-08-09 00:04:07 +0200

    added arrows

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions()
+
+
+// ***************
+
+
+
+
+const fivePlusSeven = () => 5+7
+
+fivePlusSeven();
+
+// ******************
+
+const add= (a,b) => a+b
+
+console.log(add(5,4));
+// ******************
+
+const addFive = a => a + 5
+
+
+console.log(addFive(2));
+
+// ******************
+
+const createObject = () => ({greeting: "hoi"})
+
+console.log(createObject());
